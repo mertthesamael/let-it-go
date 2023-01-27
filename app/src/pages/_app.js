d17b7dd5a306b613ed65b67df1ba696368660311
Merto <mertenercan@gmail.com>
2023-01-27 07:23:47 +0300

    -yet another font testing deployment

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -4,19 +4,27 @@ import Header from '@/layouts/Header/Header'
 import { PostContextWrapper } from '@/store/postContext'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Montserrat } from '@next/font/google'
 
 
+const montserrat = Montserrat({
+  subsets:['latin'],
+  weight:['400']
+})
 export default function App({ Component, pageProps }) {
   return(
     <WalletContextProvider>
+      <main className={montserrat.className}>
       <PostContextWrapper>
       <ChakraProvider>
         <BackgroundWrapper>
       <Header />
     <Component {...pageProps} />
+    
         </BackgroundWrapper>
     </ChakraProvider>
       </PostContextWrapper>
+      </main>
     </WalletContextProvider>
     )
 }
